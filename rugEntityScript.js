b8933d2d620d45d6b5a77959ac0d16852ea85c47
Faye Li Si Fi <lis@rose-hulman.edu>
2016-09-13 16:31:01 -0700

    print entity id

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -2,6 +2,7 @@
 	print("running rug entity script");
 	this.enterEntity = function(entityID) {
 		print("enter rug");
+		print('entityID'+entityID);
 	};
 	this.leaveEntity = function(entityID) {
 		print("leave rug");
