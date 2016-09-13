6ff1f8c9033b96b93ce9e16857add6aedb990053
Faye Li Si Fi <lis@rose-hulman.edu>
2016-09-13 12:26:14 -0700

    enter and leave entity callbacks

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -1,3 +1,9 @@
 (function(){
 	print("running rug entity script");
+	this.enterEntity = function(entityID) {
+		print("enter rug");
+	};
+	this.leaveEntity = function(entityID) {
+		print("leave rug");
+	}
 });
