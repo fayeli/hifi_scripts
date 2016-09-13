b6ad526746f2bc6c01681a826d26cd54ff048582
Faye Li Si Fi <lis@rose-hulman.edu>
2016-09-13 16:44:00 -0700

    messages

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -1,10 +1,15 @@
 (function(){
 	print("running rug entity script");
+	var handleHandMessages = function(channel, message, sender) {
+		print(message);
+	}
 	this.enterEntity = function(entityID) {
 		print("enter rug");
-		print('entityID'+entityID);
+		Messages.subscribe('Group-Teleport-'+entityID);
+		Messages.messageReceived.connect(handleMessages);
 	};
 	this.leaveEntity = function(entityID) {
 		print("leave rug");
+		Messages.unsubscribe('Group-Teleport-'+entityID);
 	}
 });
