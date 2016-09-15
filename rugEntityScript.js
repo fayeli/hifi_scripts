8efd1cf0b7406f23a538772f5cf83aa836c48e39
Faye Li Si Fi <lis@rose-hulman.edu>
2016-09-15 12:54:24 -0700

    add print messages

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -2,15 +2,18 @@
 	print("running rug entity script");
 	var handleHandMessages = function(channel, message, sender) {
 		print(message);
+		print('recieved location: ' + JSON.stringify(message));
 	}
 	this.enterEntity = function(entityID) {
 		print("enter rug");
 		Messages.subscribe('Group-Teleport-'+entityID);
+		print('subsribe to Group-Teleport-'+entityID);
 		Messages.messageReceived.connect(handleMessages);
 	};
 	this.leaveEntity = function(entityID) {
 		print("leave rug");
 		Messages.unsubscribe('Group-Teleport-'+entityID);
+		print('unsubsribe from Group-Teleport-'+entityID);
 		Messages.messageReceived.disconnect(handleMessages);
 	}
 });
