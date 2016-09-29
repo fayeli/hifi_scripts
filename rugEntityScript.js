5f40d2d6194ecf1489d5efed63af324c261e6210
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-09-28 17:13:29 -0700

    prevents leader from getting location sent by self

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -2,14 +2,17 @@
 	var myRugChannel;
 	print("running rug entity script");
 	var handleMessages = function(channel, message, sender) {
-		if (channel === myRugChannel){
-			var vec = JSON.parse(message);
-			print('recieved message, leader position: ' + JSON.stringify(vec));
-			var offset = Quat.getFront(MyAvatar.orientation);
-	        offset = Vec3.multiply(offset,0.5);
-			var newLocation = Vec3.sum(vec, offset);
-			print('Teleport avatar to new location: ' + JSON.stringify(newLocation));
-			MyAvatar.goToLocation(newLocation, false);
+		if (channel === myRugChannel) {
+			// Don't teleport again if message is sent from yourself (group leader)
+			if (sender !== MyAvatar.sessionUUID) {
+				var vec = JSON.parse(message);
+				print('recieved message, leader position: ' + JSON.stringify(vec));
+				var offset = Quat.getFront(MyAvatar.orientation);
+	    		offset = Vec3.multiply(offset,0.5);
+				var newLocation = Vec3.sum(vec, offset);
+				print('Teleport avatar to new location: ' + JSON.stringify(newLocation));
+				MyAvatar.goToLocation(newLocation, false);
+			}
 			print('After teleport, unsubsribe from ' + myRugChannel);
 			Messages.unsubscribe(myRugChannel);
 			Messages.messageReceived.disconnect(handleMessages);
