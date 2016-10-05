210bccb5bc0829cbb680e995ae0513678d2c8ec0
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-10-05 12:40:06 -0700

    smaller offset

--- rugEntityScript.js
+++ rugEntityScript.js
@@ -8,7 +8,7 @@
 				var vec = JSON.parse(message);
 				print('recieved message, leader position: ' + JSON.stringify(vec));
 				var offset = Quat.getFront(MyAvatar.orientation);
-	    		offset = Vec3.multiply(offset,0.5);
+	    		offset = Vec3.multiply(offset,0.1);
 				var newLocation = Vec3.sum(vec, offset);
 				print('Teleport avatar to new location: ' + JSON.stringify(newLocation));
 				MyAvatar.goToLocation(newLocation, false);
