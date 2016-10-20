b29c946d72f601ad5f9819788fc24fee30e2ed28
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-10-20 15:20:47 -0700

    set snap block grid

---
+++ setSnapBlockGrid.js
@@ -0,0 +1,18 @@
+// 
+//  setSnapBlockGrid.js
+//
+//  Created by Si Fi Faye Li on 20 Oct, 2016
+//
+//  This script sets the correct grid size for Zimberlab's building
+//  blocks, allowing easier building via edit mode.
+//
+
+(function(){ // BEGIN LOCAL_SCOPE
+    var confirmed = Window.confirm("Thanks for downloading Zimberlab Building Block Kit!\nWould you like to enable Snap To Grid? (Recommended for easy alignment of building blocks)");
+    if (confirmed) {
+        var channel = "Snap-Block-" + MyAvatar.sessionUUID;
+        var message = "OH SNAP";
+        Messages.sendMessage(channel, message);
+        Window.alert("Snap To Grid enabled.\nYou may turn on EDIT mode to start building, enjoy!");
+    }
+}()); // END LOCAL_SCOPE
