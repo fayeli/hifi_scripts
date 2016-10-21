eb472567486a5f85284d7aeed818619ff1378c1e
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-10-20 18:41:39 -0700

    teleports towards avatar orientation

--- teleportToLookAt.js
+++ teleportToLookAt.js
@@ -1,9 +1,12 @@
 // teleportToLookAt.js
 // Created by Faye Li on 20 Oct 2016
 //
-// This script teleports your avatar to where you are looking at
+// This script teleports your avatar to towards where you are looking at
 //
 (function(){ // BEGIN LOCAL_SCOPE
-    var posLookAt = MyAvatar.getTargetAvatarPosition();
-    MyAvatar.goToLocation(posLookAt);
+    var d = 5;
+    var vecLookAt = Quat.getFront(MyAvatar.orientation);
+    vecLookAt = Vec3.multiply(vec,d);
+    var newLocation = Vec3.sum(vecLookAt, MyAvatar.position);
+    MyAvatar.goToLocation(newLocation);
 }()); // END LOCAL_SCOPE
