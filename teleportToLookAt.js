9fc2b37fa303d0f7c74a475480259b86dea08ade
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-10-20 18:44:45 -0700

    fix typo

--- teleportToLookAt.js
+++ teleportToLookAt.js
@@ -4,9 +4,9 @@
 // This script teleports your avatar to towards where you are looking at
 //
 (function(){ // BEGIN LOCAL_SCOPE
-    var d = 5;
+    var d = 50;
     var vecLookAt = Quat.getFront(MyAvatar.orientation);
-    vecLookAt = Vec3.multiply(vec,d);
+    vecLookAt = Vec3.multiply(vecLookAt,d);
     var newLocation = Vec3.sum(vecLookAt, MyAvatar.position);
-    MyAvatar.goToLocation(newLocation);
+    MyAvatar.goToLocation(newLocation, false);
 }()); // END LOCAL_SCOPE
