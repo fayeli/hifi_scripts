aaf29d42d8d9a87b5f8fd28e2a4aced492476c14
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-10-31 16:25:13 -0700

    getting started on point line plane project

---
+++ PLP/PLPZoneEntityScript.js
@@ -0,0 +1,14 @@
+// PLPZoneEntityScript.js
+//
+// Created by Si Fi Faye Li on 31 Oct, 2016
+//
+//
+
+(function(){ // BEGINS LOCAL SCOPE
+    this.enterEntity = function(entityID) {
+        print("enter PLP Zone");
+    };
+    this.leaveEntity = function(entityID) {
+        print("leave PLP Zone");
+    };
+}); // ENDS LOCAL SCOPE
