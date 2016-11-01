e4bd02980ad39078d72a38eb34d6bd71e74f145b
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-11-01 16:00:32 -0700

    adding spheres and showing hand controllers

--- PLP/plpMain.js
+++ PLP/plpMain.js
@@ -5,20 +5,35 @@
 
 (function() { // BEGIN LOCAL SCOPE
 print("plpMain running");
+HMD.requestShowHandControllers();
+
 // some constants
 var RIGHT_HAND = 1;
 var LEFT_HAND = 0;
-
+var POINT_RADIUS = 0.04;
 function MyController(hand) {
     this.hand = hand;
     var _this = this;
-
-    this.triggerPress = function(value) {
-        _this.rawTriggerValue = value;
-    };
+    this.triggerClicked = false;
 
     this.triggerClick = function(value) {
         _this.triggerClicked = value;
+        if (value === 1) {
+            // on trigger click, create a sphere at hand position
+            // TODO: set right/left hand pos
+            var hand_position = MyAvatar.getJointPosition("RightHand");
+            var properties = {
+                type: "Sphere",
+                name: "plp_point",
+                dimensions: POINT_RADIUS,
+                position: hand_position
+            }
+            print("PLP DEBUG adding sphere at " + JSON.stringify(hand_position));
+            Entities.addEntity(properties);
+
+        }
+    }
+    this.update = function(deltaTime, timestamp) {
     }
 }
 
@@ -34,12 +49,18 @@ mapping.from([Controller.Standard.LTClick]).peek().to(leftController.triggerClic
 
 Controller.enableMapping(MAPPING_NAME);
 
+function update(deltaTime) {
+    var timestamp = Date.now();
+    leftController.update(deltaTime, timestamp);
+    rightController.update(deltaTime, timestamp);
+}
 
 function cleanup() {
     print("plp cleaning up");
     Controller.disableMapping(MAPPING_NAME);
-
+    HMD.requestHideHandControllers();
 }
 
 Script.scriptEnding.connect(cleanup);
+Script.update.connect(update);
 }()); // END LOCAL SCOPE
