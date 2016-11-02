74205fe7426666c336d207b085192b868b7827d2
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-11-02 14:19:59 -0700

    renamed to pointplotter

--- PLP/pointplotter.js
+++ PLP/pointplotter.js
@@ -1,10 +1,12 @@
-// plpMain.js
+// pointplotter.js
 //
 // Created by Faye Li Si Fi on November 1, 2016
 //
+// This script allows you to plot points by pulling the hand controller trigger. It's great for marking measurements in 3D space
+//
 
 (function() { // BEGIN LOCAL SCOPE
-print("plpMain running");
+print("point plotter running");
 HMD.requestShowHandControllers();
 
 // some constants
@@ -24,16 +26,18 @@ function MyController(hand) {
             var rotation = (_this.hand === RIGHT_HAND ? MyAvatar.getRightPalmRotation() : MyAvatar.getLeftPalmRotation());
             var right = Vec3.normalize(Quat.getRight(rotation));
             var up = Vec3.normalize(Quat.getUp(rotation));
-            var l = 0.08;
-            var right_offset = (_this.hand === RIGHT_HAND ? Vec3.multiply(l, right) : Vec3.multiply(-l, right));
-            var up_offset = Vec3.multiply(l, up);
+            var lr = 0.07;
+            var lu = 0.14;
+            var right_offset = (_this.hand === RIGHT_HAND ? Vec3.multiply(lr, right) : Vec3.multiply(-lr, right));
+            var up_offset = Vec3.multiply(lu, up);
             var total_offset = Vec3.sum(up_offset, right_offset);
             var spawn_pos = Vec3.sum(palm_pos, total_offset);
             var properties = {
                 type: "Sphere",
                 name: "plp_point",
                 dimensions: POINT_DIMENSIONS,
-                position: spawn_pos
+                position: spawn_pos,
+                userData: JSON.stringify({"grabbableKey": {"grabbable": true}})
             };
             print("PLP DEBUG adding sphere at " + JSON.stringify(spawn_pos));
             Entities.addEntity(properties);
@@ -55,6 +59,13 @@ mapping.from([Controller.Standard.LTClick]).peek().to(leftController.triggerClic
 
 Controller.enableMapping(MAPPING_NAME);
 
+// Disable Far Grab
+var channel = 'Hifi-Grab-Disable';
+var object = {farGrabEnabled : false};
+var message = JSON.stringify(object);
+Messages.sendMessage(channel, message);
+print("PLP DEBUG sending message to disable far grab");
+
 function update(deltaTime) {
     var timestamp = Date.now();
     leftController.update(deltaTime, timestamp);
@@ -62,7 +73,7 @@ function update(deltaTime) {
 }
 
 function cleanup() {
-    print("plp cleaning up");
+    print("point plotter cleaning up");
     Controller.disableMapping(MAPPING_NAME);
     HMD.requestHideHandControllers();
 }
