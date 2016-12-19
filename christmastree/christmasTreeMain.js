82787df82e696863b859fe9f0eeafa4c6225eedb
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2016-12-19 12:13:08 -0800

    refactor main to use a hashmap

--- christmastree/christmasTreeMain.js
+++ christmastree/christmasTreeMain.js
@@ -7,8 +7,10 @@
     var myChannel = "Christmas-Tree-Channel";
     var babyChristmasTree = "{9217d17c-dafb-4e7d-b139-ee9f45ad9aaf}";
     var largeChristmasTree = "{b4282f14-c86c-49df-95f9-713186d88825}";
-    var babyOrnaments = [];
     var largeOrnaments = [];
+    // baby ornament entityID : corresponding large ornament entityID map
+    var myMap = {};
+    // scale factor k - determines how many times larger the large ornmanets are relative to the baby ornaments
     var k = 16;
 
     function main() {
@@ -16,17 +18,41 @@
         Messages.subscribe(myChannel);
         Messages.messageReceived.connect(handleMessage);
 
-        searchForBabyOrnaments();
-        print(babyOrnaments);
-        createLargeOrnaments();
-        print(largeOrnaments);
+        // searchForBabyOrnaments();
+        // print(babyOrnaments);
+        // createLargeOrnaments();
+        // print(largeOrnaments);
     }
     main();
 
     function handleMessage(channel, message, sender) {
         if (channel === myChannel) {
             print("Christmas Tree Main recieved message: " + message);
+            var currentBabyOrnaments = searchForBabyOrnaments();
+            print(currentBabyOrnaments);
+            
+            // removes baby ornaments no longer near the tree
+            for (var key in myMap) {
+                if (myMap.hasOwnProperty(key)) {
+                    if (currentBabyOrnaments.indexOf(key) === -1) {
+                        // removes from scene
+                        Entities.deleteEntity(myMap[key]);
+                        // removes from hashmap
+                        delete myMap[key];
+                    }
+                }
+            }
+
+            // stores new baby ornaments that weren't already in the hashmap
+            currentBabyOrnaments.forEach(function(babyOrnamentID){
+                if (!myMap.hasOwnProperty(babyOrnamentID)) {
+                    // creates a large ornament corresonding to the baby ornament
+                    myMap[babyOrnamentID] = createLargeOrnament(babyOrnamentID);
+                }
+            });
+
             updateLargeOrnaments();
+
         }
     }
 
@@ -34,6 +60,7 @@
         var radius = 2;
         var babyChristmasTreeProps = Entities.getEntityProperties(babyChristmasTree);
         var results = Entities.findEntities(babyChristmasTreeProps.position, radius);
+        var babyOrnaments = [];
         results.forEach(function(id){
             var props = Entities.getEntityProperties(id);
             if (props.description === "christmas ornament"){
@@ -44,11 +71,14 @@
                 // Entities.editEntity(id, props);
             }
         });
+        return babyOrnaments;
     }
 
     function updateLargeOrnaments() {
-        for (var i = 0; i < babyOrnaments.length; i++) {
-            babyOrnament2LargeOrnament(babyOrnaments[i], largeOrnaments[i]);
+        for (var key in myMap) {
+            if (myMap.hasOwnProperty(key)) {
+                babyOrnament2LargeOrnament(key, myMap[key]);
+            }
         }
     }
 
@@ -62,22 +92,22 @@
         Entities.editEntity(largeOrnament, largeOrnamentProps);
     }
 
-    function createLargeOrnaments() {
-        babyOrnaments.forEach(function(babyOrnament) {
-            var babyOrnamentProps = Entities.getEntityProperties(babyOrnament);
-            var scaledPos = Vec3.multiply(babyOrnamentProps.localPosition, k);
-            //TODO: scale model dimension var scaledDims = 
-            var largeOrnamentProps = {
-                type: "Model",
-                description: "large ornament",
-                modelURL: babyOrnamentProps.modelURL,
-                parentID: largeChristmasTree,
-                localPosition: scaledPos,
-                rotation: babyOrnamentProps.rotation,
-                visible: false
-            };
-            largeOrnaments.push(Entities.addEntity(largeOrnamentProps));
-        });
+    function createLargeOrnament(babyOrnamentID) {
+        var babyOrnamentProps = Entities.getEntityProperties(babyOrnamentID);
+        var scaledPos = Vec3.multiply(babyOrnamentProps.localPosition, k);
+        var scaledDims = Vec3.multiply(babyOrnamentProps.dimensions, k);
+        var largeOrnamentProps = {
+            type: "Model",
+            description: "large ornament",
+            modelURL: babyOrnamentProps.modelURL,
+            parentID: largeChristmasTree,
+            localPosition: scaledPos,
+            rotation: babyOrnamentProps.rotation,
+            dimensions: scaledDims,
+            visible: false
+        };
+        var largeOrnamentID = Entities.addEntity(largeOrnamentProps);
+        return largeOrnamentID;
     }
 
     function reset() {
@@ -85,8 +115,14 @@
     }
 
     function clearLargeOrnaments() {
-        largeOrnaments.forEach(function(id){
-            Entities.deleteEntity(id);
+        var radius = 1000;
+        var largeChristmasTreeProps = Entities.getEntityProperties(largeChristmasTree);
+        var results = Entities.findEntities(largeChristmasTreeProps.position, radius);
+        results.forEach(function(id){
+            var props = Entities.getEntityProperties(id);
+            if (props.description === "large ornament"){
+                Entities.deleteEntity(id);
+            }
         });
     }
 
